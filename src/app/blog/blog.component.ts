357b3d63638767169b75c77b6466b535223cd4e7
Nijantha Selvam <nijantha.selvam@idp.com>
2021-12-13 11:48:16 +0530

    IP User

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent implements OnInit {
+  ipAddress = '';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  getIPAddress() {
+    this.http.get("http://api.ipify.org/?format=json").subscribe((res: any) => {
+      this.ipAddress = res.ip
+    })
+  }
+
+}
